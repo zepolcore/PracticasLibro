7c4cb5cc46ca2c643802cb5010f51d27112d4a4c
zepolcore <martinlopezutn@gmail.com>
2024-03-09 15:52:39 -0300

    first commit

---
+++ Practicas/src/caracteresEspeciales/CaracteresEspeciales.java
@@ -0,0 +1,16 @@
+
+package caracteresEspeciales;
+
+/**
+ *
+ * @author martinlopez
+ */
+public class CaracteresEspeciales {
+    public static void main(String[] args){
+
+    //para los caracteres especiaes, que se imprimen con sout, podemos pegarlos 
+    //o introducir: \u266A 
+    //son emojis que se pueden introducir
+    System.out.println("\233A");
+    }
+}
---
+++ Practicas/src/coloreadoTexto/Colores.java
@@ -0,0 +1,13 @@
+
+package coloreadoTexto;
+
+/**
+ *
+ * @author martinlopez
+ */
+public class Colores {
+    public static void main(String[] args){
+        System.out.println("\033[33m mandarina");
+        //para cambiar el color hay que cambiar el 33m del final por otro numero
+    }
+}
